fd825d7013f4e220c91048d86749c110f738c6d6
Bart <Bart>
2017-03-08 21:44:23 +0100

    Added d3

--- ac1/ac1/scripts/app-bundle.js
+++ ac1/ac1/scripts/app-bundle.js
@@ -1,4 +1,4 @@
-define('app',["require", "exports", "moment"], function (require, exports, moment) {
+define('app',["require", "exports", "moment", "d3"], function (require, exports, moment, d3) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var App = (function () {
@@ -7,6 +7,34 @@ define('app',["require", "exports", "moment"], function (require, exports, momen
         }
         App.prototype.hello = function () {
             return moment().format();
+        };
+        App.prototype.attached = function () {
+            var data = [{
+                    label: "7am",
+                    sales: 20
+                }, {
+                    label: "8am",
+                    sales: 12
+                }, {
+                    label: "9am",
+                    sales: 8
+                }, {
+                    label: "10am",
+                    sales: 27
+                }];
+            var g = d3.select('#chart')
+                .selectAll("g")
+                .data(data)
+                .enter()
+                .append('g');
+            g.append("circle")
+                .attr('cy', 40)
+                .attr('cx', function (d, i) { return (i + 1) * 50; })
+                .attr('r', function (d) { return d.sales; });
+            g.append("text")
+                .attr('y', 90)
+                .attr('x', function (d, i) { return (i + 1) * 50; })
+                .text(function (d) { return d.label; });
         };
         return App;
     }());
@@ -53,5 +81,5 @@ define('resources/index',["require", "exports"], function (require, exports) {
     exports.configure = configure;
 });
 
-define('text!app.html', ['module'], function(module) { module.exports = "<template><require from=\"bootstrap/css/bootstrap.css\"></require><h1>${message} ${hello()}</h1><div class=\"dropdown\"><button class=\"btn btn-default dropdown-toggle\" type=\"button\" id=\"dropdownMenu1\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"true\">Dropdown <span class=\"caret\"></span></button><ul class=\"dropdown-menu\" aria-labelledby=\"dropdownMenu1\"><li><a href=\"#\">Action</a></li><li><a href=\"#\">Another action</a></li><li><a href=\"#\">Something else here</a></li><li role=\"separator\" class=\"divider\"></li><li><a href=\"#\">Separated link</a></li></ul></div><button type=\"button\" class=\"btn btn-default\" aria-label=\"Left Align\"><span class=\"glyphicon glyphicon-align-left\" aria-hidden=\"true\"></span></button></template>"; });
+define('text!app.html', ['module'], function(module) { module.exports = "<template><style>#chart div{display:inline-block;background:#4285f4;width:20px;margin-right:3px}</style><require from=\"bootstrap/css/bootstrap.css\"></require><h1>${message} ${hello()}</h1><svg id=\"chart\"></svg><div class=\"dropdown\"><button class=\"btn btn-default dropdown-toggle\" type=\"button\" id=\"dropdownMenu1\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"true\">Dropdown <span class=\"caret\"></span></button><ul class=\"dropdown-menu\" aria-labelledby=\"dropdownMenu1\"><li><a href=\"#\">Action</a></li><li><a href=\"#\">Another action</a></li><li><a href=\"#\">Something else here</a></li><li role=\"separator\" class=\"divider\"></li><li><a href=\"#\">Separated link</a></li></ul></div><button type=\"button\" class=\"btn btn-default\" aria-label=\"Left Align\"><span class=\"glyphicon glyphicon-align-left\" aria-hidden=\"true\"></span></button></template>"; });
 //# sourceMappingURL=app-bundle.js.map
--- ac1/ac1/src/app.ts
+++ ac1/ac1/src/app.ts
@@ -1,12 +1,47 @@
 import * as moment from 'moment';
+import * as d3 from 'd3'
 
 export class App {
-  message = 'Hello World! 2';
+    message = 'Hello World! 2';
 
-   constructor(){
+    constructor() {
+
+
+
     }
 
-    hello() : string {
+    hello(): string {
         return moment().format();
     }
+
+    attached() {
+        const data = [{
+  label: "7am",
+  sales: 20
+},{
+  label: "8am",
+  sales: 12
+}, {
+  label: "9am",
+  sales: 8
+}, {
+  label: "10am",
+  sales: 27
+}]
+
+      const g = d3.select('#chart')
+  .selectAll("g")
+  .data(data)
+  .enter()
+  .append('g')
+g.append("circle")
+  .attr('cy', 40)
+  .attr('cx', (d, i)=> (i+1) * 50)
+  .attr('r', (d)=> d.sales)
+g.append("text")
+  .attr('y', 90)
+  .attr('x', (d, i)=> (i+1) * 50)
+  .text((d)=> d.label)
+    }
+
 }
